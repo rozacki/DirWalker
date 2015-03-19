4046857bca09a5c4781f745438ab36d18afee813
rozackic <chris.rozacki@valtech.co.uk>
2015-03-19 19:47:41 +0000

    logging to a file, commande line parameters added: -nic=network, -path=url path, -port=tcp port, -debug=true|false

--- main.go
+++ main.go
@@ -1,30 +1,31 @@
 package main
-/*
-Todo:unit tests and benchmarks
 
+/*
 havealook:gerkin goconvey
 
-fileptah.EvalSymLinks
- */
+Todo:unit tests and benchmarks
+Todo:error handling
+Todo:graceful shutdown via signals
+*/
 import (
+	"flag"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
-	"reflect"
-	"io/ioutil"
 	_ "sort"
+	"strconv"
 )
 
 //all types of resources we can be dealing with
 const (
-	AppName						string="DirWalker"
+	AppName                    string = "DirWalker"
 	DefaultTemplateString      string = "<td>{{.Mode}}</td><td>{{.Name}}</td><td>{{.Size}}</td><td>{{.ModTime}}</td>"
-	DefaultTemplateName        string = "x"//unique name, if user want he can override default template with x template name
-	FileTemplateName		   string="-"
+	DefaultTemplateName        string = "x" //unique name, if user want he can override default template with x template name
+	FileTemplateName           string = "-"
 	ModeDirTemplateName        string = "d"
 	ModeExclusiveTemplateName  string = "l"
 	ModeTemporaryTemplateName  string = "T"
@@ -37,12 +38,33 @@ const (
 	ModeCharDeviceTemplateName string = "c"
 	ModeStickyTemplateName     string = "t"
 
-	StartingFolder 				string = "/Users/chrisrozacki/Desktop/music/brian  eno/brian eno - 1973 here come the warm jets"
-	HeaderTemplateName			string="header"
-	FooterTemplateName			string="footer"
-	ItemStartTemplateName		string="item_start"
-	ItemEndTemplateName			string="item_end"
+	StartingFolder        string = "/Users/chrisrozacki/Desktop/music/brian  eno/brian eno - 1973 here come the warm jets"
+	HeaderTemplateName    string = "header"
+	FooterTemplateName    string = "footer"
+	ItemStartTemplateName string = "item_start"
+	ItemEndTemplateName   string = "item_end"
+
+	ModTimelayout = "Jan 2, 2006 at 3:04pm"
+
+	//error code
+	//log file path
+	LogFileName	string	="log.txt"
 )
+//error codes
+const(
+
+)
+//error type
+type Error struct{
+	Op string
+	Err error
+	Port int
+	Path string
+}
+//error type implemtns Error() method
+func (err Error) Error() string{
+	return fmt.Sprintf("error: %s while listen and server for port %d",err.Err.Error(),err.Port)
+}
 
 //walker mainly keeps
 type DirWalker struct {
@@ -60,13 +82,13 @@ func CreateDirWalker(debug bool) DirWalker {
 
 	//
 	walker.Templates = make(map[string]*template.Template)
-	walker.Templates=map[string]*template.Template{
+	walker.Templates = map[string]*template.Template{
 		//use Stringer and provide template key
 		// d---------
-		FileTemplateName:	nil,
-		os.ModeDir.String()[:1]:	nil,
-		os.ModeAppend.String()[:1]: nil,
-		os.ModeExclusive.String()[:1]:	nil,
+		FileTemplateName:               nil,
+		os.ModeDir.String()[:1]:        nil,
+		os.ModeAppend.String()[:1]:     nil,
+		os.ModeExclusive.String()[:1]:  nil,
 		os.ModeTemporary.String()[:1]:  nil,
 		os.ModeSymlink.String()[:1]:    nil,
 		os.ModeDevice.String()[:1]:     nil,
@@ -76,10 +98,10 @@ func CreateDirWalker(debug bool) DirWalker {
 		os.ModeSetgid.String()[:1]:     nil,
 		os.ModeCharDevice.String()[:1]: nil,
 		os.ModeSticky.String()[:1]:     nil,
-		FooterTemplateName:nil,
-		HeaderTemplateName:nil,
-		ItemStartTemplateName:nil,
-		ItemEndTemplateName:nil,
+		FooterTemplateName:             nil,
+		HeaderTemplateName:             nil,
+		ItemStartTemplateName:          nil,
+		ItemEndTemplateName:            nil,
 	}
 
 	//parse the default template of error then panic
@@ -95,7 +117,7 @@ func CreateDirWalker(debug bool) DirWalker {
 			log.Print(err)
 			log.Print("default template used")
 			//reuse compiled template
-			t= defaultTemplate
+			t = defaultTemplate
 		} else {
 			log.Print("templated loaded: ", key)
 		}
@@ -107,6 +129,17 @@ func CreateDirWalker(debug bool) DirWalker {
 	return walker
 }
 
+func (self DirWalker) Start(urlPath string,nic string, port int) error{
+	http.Handle(urlPath, &self)
+
+	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
+
+	if err != nil {
+		return Error{Op:"listen and serve",Err:err,Port:port}
+	}
+	return nil
+}
+
 func (self *DirWalker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 
@@ -114,29 +147,29 @@ func (self *DirWalker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if _, ok := r.URL.Query()["debug"]; ok {
 		debug = true
 	}
-	dir:="/"
-	dirs:=r.URL.Query()["dir"]
-	if dirs!=nil{
-		dir=filepath.Clean(dirs[0])
-		log.Printf("dir=%s\n",dir)
+	dir := "/"
+	dirs := r.URL.Query()["dir"]
+	if dirs != nil {
+		dir = filepath.Clean(dirs[0])
+		log.Printf("dir=%s\n", dir)
 	}
-	items,err:=	ioutil.ReadDir(dir)
-	if err!=nil{
-		if items,err=ioutil.ReadDir("/");err!=nil{
+	items, err := ioutil.ReadDir(dir)
+	if err != nil {
+		if items, err = ioutil.ReadDir("/"); err != nil {
 			panic(err)
 		}
 	}
 
-	dirs=	make([]string,0)
-	SplitPath(dir,&dirs)
+	dirs = make([]string, 0)
+	SplitPath(dir, &dirs)
 
-	infoMap:=map[string]interface{}{
-		"Path":dir,
-		"Paths":dirs,
+	infoMap := map[string]interface{}{
+		"Path":  dir,
+		"Paths": dirs,
 	}
-	self.Templates[HeaderTemplateName].Execute(w,infoMap)
+	self.Templates[HeaderTemplateName].Execute(w, infoMap)
 
-	for _,info:= range	items {
+	for _, info := range items {
 		log.Printf("%+v\n", info)
 		log.Println(err)
 
@@ -146,17 +179,17 @@ func (self *DirWalker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		//convert struct to map and send it to the template
 		infoMap := map[string]interface{}{
-		"Name":    	info.Name(),
-		"Size":    	strconv.FormatInt(info.Size(), 10),
-		"IsDir":   	strconv.FormatBool(info.IsDir()),
-		"Mode":    	info.Mode().String(),
-		"ModTime": 	info.ModTime().Format(ModTimelayout),
-		"Path": 	filepath.Join(dir,info.Name()),
+			"Name":    info.Name(),
+			"Size":    strconv.FormatInt(info.Size(), 10),
+			"IsDir":   strconv.FormatBool(info.IsDir()),
+			"Mode":    info.Mode().String(),
+			"ModTime": info.ModTime().Format(ModTimelayout),
+			"Path":    filepath.Join(dir, info.Name()),
 		}
 
 		var tmpl *template.Template = nil
 		var key string = DefaultTemplateName
-		var ok    bool
+		var ok bool
 		//if key exist
 		//use key and template
 		//else
@@ -175,59 +208,75 @@ func (self *DirWalker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			//assign the one that always should be available
 			tmpl = self.Templates[DefaultTemplateName]
 		}
-		err=self.Templates[ItemStartTemplateName].Execute(w,nil)
+		err = self.Templates[ItemStartTemplateName].Execute(w, nil)
 		err = tmpl.Execute(w, infoMap)
-		err=self.Templates[ItemEndTemplateName].Execute(w,nil)
+		err = self.Templates[ItemEndTemplateName].Execute(w, nil)
 	}
-	self.Templates[FooterTemplateName].Execute(w,nil)
+	self.Templates[FooterTemplateName].Execute(w, nil)
 	log.Println(err)
 }
 
-const ModTimelayout = "Jan 2, 2006 at 3:04pm"
-
-func SplitPath(path string,dirs* []string){
-	*dirs=append(*dirs,path)
-	log.Println(path," ", len(path))
+func SplitPath(path string, dirs *[]string) {
+	*dirs = append(*dirs, path)
+	log.Println(path, " ", len(path))
 	//if nothing left then we stop
-	if len(path)<=1{
-		if len(path)==0 {
+	if len(path) <= 1 {
+		if len(path) == 0 {
 			*dirs = append(*dirs, "/")
 		}
-		return;
+		return
 	}
-	element:=filepath.Base(path)
+	element := filepath.Base(path)
 	fmt.Println(element)
 	//starting index:ending index
-	path=path[0:(len(path)-len(element))-1]
-	SplitPath(path,dirs)
+	path = path[0 : (len(path)-len(element))-1]
+	SplitPath(path, dirs)
 }
 
+func SetLogFile(logPath string ) error{
 
-func PrintFileModes(){
-	/*fmt.Println(os.ModeAppend,os.ModeExclusive,os.ModeTemporary,
-		os.ModeSymlink,os.ModeDevice,
-		os.ModeNamedPipe,os.ModeSocket,os.ModeSetuid,
-		os.ModeSetgid,os.ModeCharDevice,os.ModeSticky, os.ModeType, os.ModePerm)
-		*/
-	fmt.Print(os.ModeDir.String()[0:1])
+	f, err:=os.Create(logPath)
+	if err!=nil{
+		return Error{Op:"file open",Err:err,Path:logPath}
+	}
+	defer f.Close()
 
-	fmt.Printf("%+v\n",reflect.TypeOf(os.ModeDir))
-	//fmt.Printf("%+v\n",reflect.TypeOf(os.FileMode))
+	log.SetOutput(f)
+	return nil
 }
-
 
 func main() {
-	PrintFileModes();
+
+	//set log file
+	SetLogFile(LogFileName)
 
 	log.Println("staring ", AppName)
 
-	dw := CreateDirWalker(true)
+	//wait for signals
+	//.....
 
-	http.Handle("/", &dw)
+	//parse command line parameters
+	nic := flag.String("nic", "localhost", "")
+	port := flag.Int("port", 8080, "")
+	debug := flag.Bool("debug", false, "")
+	urlPath:=flag.String("path","/","")
+	flag.Parse()
 
-	err := http.ListenAndServe(":8080", nil)
+	log.Println("interface=", *nic)
+	log.Println("port=", *port)
+	log.Println("debug=", *debug)
+	log.Println("path=",*urlPath)
 
-	if err != nil {
-		log.Panic(err)
+	//initialize the mani structure
+	dw := CreateDirWalker(*debug)
+
+	//star the server
+	err:=	dw.Start(*urlPath ,*nic , *port)
+	if err!=nil{
+		log.Println(err)
+		log.Println("server will stop")
+		return
 	}
+
+	log.Println("server started")
 }
